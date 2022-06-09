8908cb26e2fc2a9bf372ccea994a7dd8ffc5c218
Ali Zahir <Ali>
2022-06-09 14:36:05 -0400

    adds data dog

--- src/index.js
+++ src/index.js
@@ -4,6 +4,24 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import { datadogRum } from '@datadog/browser-rum';
+
+datadogRum.init({
+    applicationId: '8f1ad3a0-3b17-47ad-85c9-75e8cc95605d',
+    clientToken: 'pub943902bca4a1ad8c1fe9e6ef5c8f366a',
+    site: 'datadoghq.com',
+    service:'sample-app',
+    
+    // Specify a version number to identify the deployed version of your application in Datadog 
+    version: '1.0.0',
+    sampleRate: 100,
+    premiumSampleRate: 100,
+    trackInteractions: true,
+    defaultPrivacyLevel:'mask-user-input'
+});
+    
+datadogRum.startSessionReplayRecording();
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
